fdadbd7d31334c7a887cf3fa0b1e85415633191f
Aymenbz <aymenweb5@gmail.com>
2020-04-10 17:08:31 +0100

    here we go

---
+++ src/App.js
@@ -0,0 +1,74 @@
+import React, { useEffect, useState } from "react";
+import { Route } from "react-router-dom";
+import gsap from "gsap";
+import "./styles/App.scss";
+import Header from "./components/header";
+
+// Pages
+import Home from "./pages/home";
+import CaseStudies from "./pages/caseStudies";
+import Services from "./pages/services";
+import Approach from "./pages/approach";
+import About from "./pages/about";
+import Navigation from "./components/navigation";
+
+// routes
+
+const routes = [
+  { path: "/", name: "Home", Component: Home },
+  { path: "/case-studies", name: "Case Studies", Component: CaseStudies },
+  { path: "/approach", name: "Approach", Component: Approach },
+  { path: "/services", name: "Services", Component: Services },
+  { path: "/about-us", name: "About Us", Component: About },
+];
+
+function debounce(fn, ms) {
+  let timer;
+  return () => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      timer = null;
+      fn.apply(this, arguments);
+    }, ms);
+  };
+}
+
+function App() {
+  gsap.to("body", 0, { css: { visibility: "visible" } });
+  const [dimensions, setDimensions] = useState({
+    height: window.innerHeight,
+    width: window.innerWidth,
+  });
+  useEffect(() => {
+    let vh = dimensions.height * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+
+    const debouncedHandleResize = debounce(function handleResize() {
+      setDimensions({
+        height: window.innerHeight,
+        width: window.innerWidth,
+      });
+    }, 1000);
+
+    window.addEventListener("resize", debouncedHandleResize);
+
+    return () => {
+      window.removeEventListener("resize", debouncedHandleResize);
+    };
+  });
+  return (
+    <div>
+      <Header dimensions={dimensions} />
+      <div className="App">
+        {routes.map(({ path, Component }) => (
+          <Route key={path} exact path={path}>
+            <Component />
+          </Route>
+        ))}
+      </div>
+      <Navigation />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/header.js
@@ -0,0 +1,159 @@
+import React, { useEffect, useState } from "react";
+import { NavLink, withRouter } from "react-router-dom";
+import gsap from "gsap";
+import { ReactComponent as UpArrow } from "../assets/up-arrow-circle.svg";
+
+let tl = gsap.timeline();
+
+function Header({ history, dimensions }) {
+  const [menuState, setMenuState] = useState({
+    menuOpened: false,
+  });
+  useEffect(() => {
+    //Listening for page changes.
+    history.listen(() => {
+      setMenuState({ menuOpened: false });
+    });
+
+    if (menuState.menuOpened === true) {
+      // Run Open Menu animation
+      tl.to("body", { duration: 0.01, css: { overflow: "hidden" } })
+        .to(".App", {
+          duration: 1,
+          y: dimensions.width <= 654 ? "70vh" : dimensions.height / 2,
+          ease: "expo.inOut",
+        })
+        .to(".hamburger-menu span", {
+          duration: 0.6,
+          delay: -1,
+          scale: 0,
+          transformOrigin: "50% 0",
+          ease: "expo.inOut",
+        })
+        .to("#Path_1", {
+          duration: 0.4,
+          delay: -0.6,
+          css: {
+            strokeDashoffset: 10,
+            strokeDasharray: 5,
+          },
+        })
+        .to("#Path_2", {
+          duration: 0.4,
+          delay: -0.6,
+          css: {
+            strokeDashoffset: 10,
+            strokeDasharray: 20,
+          },
+        })
+        .to("#Line_1", {
+          duration: 0.4,
+          delay: -0.6,
+          css: {
+            strokeDashoffset: 40,
+            strokeDasharray: 18,
+          },
+        })
+        .to("#circle", {
+          duration: 0.6,
+          delay: -0.8,
+          css: {
+            strokeDashoffset: 0,
+          },
+        })
+
+        .to(".hamburger-menu-close", {
+          duration: 0.6,
+          delay: -0.8,
+          css: {
+            display: "block",
+          },
+        });
+    } else {
+      // Run close Menu animation
+      tl.to(".App", {
+        duration: 1,
+        y: 0,
+        ease: "expo.inOut",
+      })
+        .to("#circle", {
+          duration: 0.6,
+          delay: -0.6,
+          css: {
+            strokeDashoffset: -193,
+            strokeDasharray: 227,
+          },
+        })
+        .to("#Path_1", {
+          duration: 0.4,
+          delay: -0.6,
+          css: {
+            strokeDashoffset: 10,
+            strokeDasharray: 10,
+          },
+        })
+        .to("#Path_2", {
+          duration: 0.4,
+          delay: -0.6,
+          css: {
+            strokeDashoffset: 10,
+            strokeDasharray: 10,
+          },
+        })
+        .to("#Line_1", {
+          duration: 0.4,
+          delay: -0.6,
+          css: {
+            strokeDashoffset: 40,
+            strokeDasharray: 40,
+          },
+        })
+        .to(".hamburger-menu span", {
+          duration: 0.6,
+          delay: -0.6,
+          scale: 1,
+          transformOrigin: "50% 0",
+          ease: "expo.inOut",
+        })
+        .to(".hamburger-menu-close", {
+          duration: 0,
+          css: {
+            display: "none",
+          },
+        })
+        .to("body", {
+          css: {
+            overflow: "auto",
+          },
+        });
+    }
+  }, [menuState.menuOpened]);
+  return (
+    <div className="header">
+      <div className="container">
+        <div className="row v-center space-between">
+          <div className="logo">
+            <NavLink to="/">AGENCY.</NavLink>
+          </div>
+          <div className="nav-toggle">
+            <div
+              className="hamburger-menu"
+              onClick={() => setMenuState({ menuOpened: true })}
+            >
+              <span></span>
+              <span></span>
+            </div>
+            <div
+              className="hamburger-menu-close"
+              onClick={() => setMenuState({ menuOpened: false })}
+            >
+              <UpArrow />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default withRouter(Header);
